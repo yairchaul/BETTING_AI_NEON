906651747a773663d98717193199dab1d4e31238
yairchaul <yair.chaul@gmail.com>
2026-03-11 14:15:55 -0600

    🧹 Limpieza completa: eliminados archivos obsoletos (processors/, data/, sports/, test_*). Mantenidos solo archivos esenciales. Agregado n8n_workflow.json y visual_nba_old.py

--- processors/__init__.py
+++
@@ -1,7 +0,0 @@
-﻿"""
-Módulo de procesadores para BETTING_AI
-"""
-from .procesador_futbol import ProcesadorFutbol, procesar_futbol
-from .vision_reader import VisionReader
-
-__all__ = ['ProcesadorFutbol', 'procesar_futbol', 'VisionReader']
